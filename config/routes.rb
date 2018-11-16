c79e0662ae7dc35f526cade9e7040d60ffc9dc7d
日野恵太 <keitahino0415@gmail.com>
2018-11-16 14:39:20 +0900

    add:search用のルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/callback' => 'linebot#callback'
   get '/new' => 'linebot#new'
+  get '/search' => 'linebot#search'
   post '/foodrecords' => 'linebot#create'
 end
