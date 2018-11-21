50cac65cef918f7fb11ea3b51f95c0d58cc4ad34
日野恵太 <keitahino0415@gmail.com>
2018-11-21 16:40:25 +0900

    feat:food_searchメソッド追加

--- app/models/foodrecord.rb
+++ app/models/foodrecord.rb
@@ -1,3 +1,17 @@
 class Foodrecord < ApplicationRecord
+  mount_uploader :picture, PictureUploader
   belongs_to :lineuser, foreign_key: 'userid', class_name: 'Lineuser',optional: true
+
+  def self.food_search(lineuser_id:,store_name:,menu_name:,taste:,thickness:,hardness:,taste_intensity:,evalute:)
+    q = Foodrecord.where(lineuser_id: lineuser_id)
+    q = q.where(store_name: store_name) if store_name.present?
+    q = q.where(menu_name: menu_name) if menu_name.present?
+    q = q.where(taste: taste) if taste.present?
+    q = q.where(thickness: thickness) if thickness.present?
+    q = q.where(hardness: hardness) if hardness.present?
+    q = q.where(taste_intensity: taste_intensity) if taste_intensity.present?
+    q = q.where(evalute: evalute) if evalute.present?
+    return q
+  end
+
 end
