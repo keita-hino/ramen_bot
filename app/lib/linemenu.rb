5a56986cb0ecb72a6f884f0832f4243f9c0b4f06
日野恵太 <keitahino0415@gmail.com>
2018-11-23 20:56:23 +0900

    feat:share_replyメソッド追加

--- app/lib/linemenu.rb
+++ app/lib/linemenu.rb
@@ -35,7 +35,8 @@ class Linemenu
   end
 
   def self.search_result(food)
-    {
+    a = []
+    a.push ({
     type: "flex",
       altText: "this is a flex message",
       contents: {
@@ -240,7 +241,195 @@ class Linemenu
             }
         end
       }
-    }
+    })
+
+    a.push ({
+    type: "flex",
+      altText: "this is a flex message",
+      contents: {
+        type: 'carousel',
+        contents:
+          food.map do |v|
+            {
+              type: 'bubble',
+              hero: {
+                type: 'image',
+                size: 'full',
+                aspectRatio: '20:13',
+                url: ENV['RAMEN_PICTURE_PATH'] + v['id'].to_s + "/" + v['picture'].to_s
+              },
+              body: {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'xl',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '　　　　店名',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['store_name'],
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'lg',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '　　メニュー',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['menu_name'],
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'lg',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '　スープの味',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['taste'],
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'lg',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '　　麺の太さ',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['thickness'],
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'lg',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '　　麺の硬さ',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['hardness'],
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'lg',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: 'スープの濃さ',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['taste_intensity'],
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'horizontal',
+                    margin: 'lg',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: '　　　評価',
+                        color: '#aaaaaa',
+                        size: 'md',
+                        align: 'center',
+                        flex: 1
+                      },
+                      {
+                        type: 'text',
+                        text: v['evalute'].to_s,
+                        wrap: true,
+                        weight: 'bold',
+                        size: 'md',
+                        flex: 1
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+        end
+      }
+    })
+
   end
 
   def self.share_reply(user)
