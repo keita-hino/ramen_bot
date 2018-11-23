8e5c27ff8afa49c11c04e4045eb427d5fd935d7f
日野恵太 <keitahino0415@gmail.com>
2018-11-23 19:14:28 +0900

    add:Tempモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122050405) do
+ActiveRecord::Schema.define(version: 20181123040507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20181122050405) do
 
   create_table "lineusers", force: :cascade do |t|
     t.text "userid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "temps", force: :cascade do |t|
+    t.text "userid"
+    t.jsonb "payload"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
