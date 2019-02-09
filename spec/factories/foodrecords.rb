636bf2091a401d7201225194fa59e5957680cbb1
日野恵太 <keitahino0415@gmail.com>
2019-02-09 09:19:54 +0900

    add:ファクトリー追加

---
+++ spec/factories/foodrecords.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :foodrecord do
+    store_name{"store_name"}
+    menu_name{"menu_name"}
+    taste{"taste"}
+    thickness{"thickness"}
+    hardness{"hardness"}
+    taste_intensity{"taste_intensity"}
+    evalute{0.5}
+    lineuser_id{"lineuser_id"}
+    picture{"768E89EF-D231-425C-A779-C2F5D91DC7AE.jpeg"}
+  end
+end
