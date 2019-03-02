5a649c34a2dbfeefe1deebb976b98a73ea82f0d5
日野恵太 <keitahino0415@gmail.com>
2019-03-02 15:57:55 +0900

    add:food_searchメソッドのテストケース追加

---
+++ spec/models/foodrecord_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Foodrecord, type: :model do
+  before do
+    FactoryBot.create(:foodrecord)
+  end
+  describe '#food_search' do
+    it '1件返る' do
+      expect(Foodrecord.food_search(lineuser_id:"lineuser_id",store_name:"store_name",menu_name:"menu_name",taste:"taste",thickness:"thickness",hardness:"hardness",taste_intensity:"taste_intensity",evalute:0.5).count).to eq(1)
+    end
+  end
+
+
+end
