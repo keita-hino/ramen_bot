79d3a326c5ae81cd160b7cac493fe3262ea152f5
日野恵太 <keitahino0415@gmail.com>
2019-05-01 20:35:33 +0900

    add:バリデーションのテスト追加

--- spec/models/foodrecord_spec.rb
+++ spec/models/foodrecord_spec.rb
@@ -2,8 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Foodrecord, type: :model do
   before do
-    FactoryBot.create(:foodrecord)
+    @foodrecord = FactoryBot.create(:foodrecord)
   end
+  describe 'validate' do
+    describe 'store_name' do
+      context '店名が未入力の場合' do
+        it 'エラーを返す' do
+          @foodrecord.store_name = nil
+          expect(@foodrecord).not_to be_valid
+        end
+      end
+    end
+
+    describe 'menu_name' do
+      context 'メニューが未入力の場合' do
+        it 'エラーを返す' do
+          @foodrecord.menu_name = nil
+          expect(@foodrecord).not_to be_valid
+        end
+      end
+    end
+  end
+
   describe '#food_search' do
     it '1件返る' do
       expect(Foodrecord.food_search(lineuser_id:"lineuser_id",store_name:"store_name",menu_name:"menu_name",taste:"taste",thickness:"thickness",hardness:"hardness",taste_intensity:"taste_intensity",evalute:0.5).count).to eq(1)
